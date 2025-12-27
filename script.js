f562179ecaa5798d849ba8c5ebcb6aabcac1785c
POOJA ISHWARYA SELVI.R <pooja20061028@gmail.com>
2025-12-27 17:48:58 +0530

    Added Task 2 – Stopwatch Web Application

--- script.js
+++ script.js
@@ -1,8 +1,46 @@
-window.addEventListener("scroll", function () {
-    const navbar = document.getElementById("navbar");
-    if (window.scrollY > 50) {
-        navbar.classList.add("scrolled");
-    } else {
-        navbar.classList.remove("scrolled");
-    }
-});
+let hours = 0, minutes = 0, seconds = 0;
+let timerDisplay = document.getElementById("timer");
+let interval = null;
+
+function updateDisplay() {
+    let h = hours < 10 ? "0" + hours : hours;
+    let m = minutes < 10 ? "0" + minutes : minutes;
+    let s = seconds < 10 ? "0" + seconds : seconds;
+
+    timerDisplay.textContent = `${h}:${m}:${s}`;
+}
+
+document.getElementById("start").onclick = () => {
+    if (interval) return;
+
+    interval = setInterval(() => {
+        seconds++;
+
+        if (seconds === 60) {
+            seconds = 0;
+            minutes++;
+        }
+        if (minutes === 60) {
+            minutes = 0;
+            hours++;
+        }
+
+        updateDisplay();
+    }, 1000);
+};
+
+document.getElementById("stop").onclick = () => {
+    clearInterval(interval);
+    interval = null;
+};
+
+document.getElementById("reset").onclick = () => {
+    clearInterval(interval);
+    interval = null;
+
+    hours = 0;
+    minutes = 0;
+    seconds = 0;
+
+    updateDisplay();
+};
